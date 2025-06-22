ed346c94cca5d13082d451260fdf4e22b7098f5b
Shane <palomoshanecraven@gmail.com>
2025-06-22 17:46:53 +0800

    Initializing needed variables.

---
+++ bank.py
@@ -0,0 +1,11 @@
+# Includes all the information and etc.
+
+class Bank:
+    
+    def __init__(self, title, amount, type, note = ""):
+        self.title = title
+        self.amount = amount
+        self.type = type
+        self.note = note
+
+    
