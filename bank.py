b35d6b308ac178bf22254373e778e164548d376c
Shane <palomoshanecraven@gmail.com>
2025-06-22 18:10:29 +0800

    Add search wallet function.

--- bank.py
+++ bank.py
@@ -21,11 +21,16 @@ class Bank:
     # Display all
     def display_information(self):
         if not self.wallet:
-            return "No transaction has been made in your bank."
+            return f"No transaction has been made in your bank."
         return f"\n".join([Transaction.display_info() for transaction in self.wallet])
     
     # Search
-
+    def search_wallet(self, query):
+        found = [trans for trans in self.wallet if query.lower() in trans.title.lower() or query.lower() in trans.type.lower()]
+        if not found:
+            return f"No transaction has been made."
+        return "\n".join([Transaction.display_info() for transaction in found])
+    
     # Save 
 
     # Load
