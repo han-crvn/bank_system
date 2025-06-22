41242a2152cdabc366cf4324fff06291df8d7540
Shane <palomoshanecraven@gmail.com>
2025-06-22 17:51:53 +0800

    Moving def function from bank to transaction.

---
+++ transaction.py
@@ -0,0 +1,12 @@
+# Includes all the transaction that are availables.
+
+class Transaction:
+    
+    def __init__(self, title, amount, type, note = ""):
+        self.title = title
+        self.amount = amount
+        self.type = type
+        self.note = note
+
+    def display_info(self):
+        return "Transaction:\n Expense: {self.title}\n Amount: {self.amount}\n Type: {self.type}\n Note: {self.note}"
