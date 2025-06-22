dc33cdebc669fc8dd5d8d4fd9f92f64023113498
Shane <palomoshanecraven@gmail.com>
2025-06-22 18:26:38 +0800

    Import bank to main and add menu.

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 # This part includes the options and in this part we can run the program.
+from bank import Bank
 
 def main():
-    pass
+    
+    while True:
+        print("\n===== Personal Bank System =====")
+        print("1. Add a Transaction")
+        print("2. Remove a Transaction")
+        print("3. Display All Transaction")
+        print("4. Search for a Transaction")
+        print("5. Save a Transaction")
+        print("6. Load Transaction from file")
+        print("7. Exit Program")
 
+        choice = input("Enter your choice (1-7): ")
 if __name__ in "__main__":
     pass
