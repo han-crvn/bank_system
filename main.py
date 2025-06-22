af8750a8314a507cb0711202ec63b4ee45409400
Shane <palomoshanecraven@gmail.com>
2025-06-22 18:36:02 +0800

    Add options and run program.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # This part includes the options and in this part we can run the program.
 from bank import Bank
+from transaction import Transaction
+
 
 def main():
-    
+    wallet = Bank()
     while True:
         print("\n===== Personal Bank System =====")
         print("1. Add a Transaction")
@@ -14,5 +16,40 @@ def main():
         print("7. Exit Program")
 
         choice = input("Enter your choice (1-7): ")
+
+        if choice == "1":
+            title = input("Enter the title: ")
+            amount = float(input("Enter amount: "))
+            type = input("Expense or Deposit: ")
+            action = Transaction(title, amount, type)
+            wallet.add_transaction(action)
+
+        elif choice == "2":
+            title = input("Enter the title: ")
+            print(wallet.delete_transaction(title))
+
+        elif choice == "3":
+            print(wallet.display_information())
+
+        elif choice == "4":
+            query = input("Enter the title: ")
+            print(wallet.search_wallet(query))
+
+        elif choice == "5":
+            wallet.save_information()
+            print("Successfully saved!")
+
+        elif choice == "6":
+            wallet.load_bank()
+            print("Loaded json file.")
+
+        elif choice == "7":
+            print("Leaving the program.")
+            break
+
+        else:
+            print("Invalid input. Please try again.")
+
+
 if __name__ in "__main__":
-    pass
+    main()
