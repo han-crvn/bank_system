3ba7379a3476c5d75d18e3b7fec1991dc3228825
Shane <palomoshanecraven@gmail.com>
2025-06-22 18:52:44 +0800

    Add spacing and handling error.

--- main.py
+++ main.py
@@ -6,55 +6,59 @@ from transaction import Transaction
 def main():
     wallet = Bank()
     while True:
-        print("\n===== Personal Bank System =====")
-        print("1. Add a Transaction")
-        print("2. Remove a Transaction")
-        print("3. Display All Transaction")
-        print("4. Search for a Transaction")
-        print("5. Save a Transaction")
-        print("6. Load Transaction from file")
-        print("7. Exit Program")
+        try:
+            print("\n===== Personal Bank System =====")
+            print("1. Add a Transaction")
+            print("2. Remove a Transaction")
+            print("3. Display All Transaction")
+            print("4. Search for a Transaction")
+            print("5. Save a Transaction")
+            print("6. Load Transaction from file")
+            print("7. Exit Program")
 
-        choice = input("Enter your choice (1-7): ")
+            choice = input("\nEnter your choice (1-7): ")
 
-        if choice == "1":
-            title = input("Enter the title: ")
-            
-            try:
-                amount = float(input("Enter amount: "))
-            except ValueError:
-                print("Invalid amount entered.")
-                continue
+            if choice == "1":
+                title = input("\nEnter the title: ")
+                
+                try:
+                    amount = float(input("\nEnter amount: "))
+                except ValueError:
+                    print("\nInvalid amount entered.")
+                    continue
 
-            type = input("Expense or Deposit: ")
-            action = Transaction(title, amount, type)
-            wallet.add_transaction(action)
+                type = input("\nExpense or Deposit: ")
+                action = Transaction(title, amount, type)
+                wallet.add_transaction(action)
 
-        elif choice == "2":
-            title = input("Enter the title: ")
-            print(wallet.delete_transaction(title))
+            elif choice == "2":
+                title = input("\nEnter the title: ")
+                print(wallet.delete_transaction(title))
 
-        elif choice == "3":
-            print(wallet.display_information())
+            elif choice == "3":
+                print(wallet.display_information())
 
-        elif choice == "4":
-            query = input("Enter the title: ")
-            print(wallet.search_wallet(query))
+            elif choice == "4":
+                query = input("\nEnter the title: ")
+                print(wallet.search_wallet(query))
 
-        elif choice == "5":
-            wallet.save_information()
-            print("Successfully saved!")
+            elif choice == "5":
+                wallet.save_information()
+                print("\nSuccessfully saved!")
 
-        elif choice == "6":
-            wallet.load_bank()
-            print("Loaded json file.")
+            elif choice == "6":
+                wallet.load_bank()
+                print("\nLoaded json file.")
 
-        elif choice == "7":
-            print("Leaving the program.")
-            break
+            elif choice == "7":
+                print("\nLeaving the program.")
+                break
 
-        else:
-            print("Invalid input. Please try again.")
+            else:
+                print("\nInvalid input. Please try again.")
+
+        except ValueError:
+            print("\nnvalid input. Please try again.")
 
 if __name__ == "__main__":
     main()
