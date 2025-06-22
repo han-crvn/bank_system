24866f8894ba1e4563e311b1bda2406a17795974
Shane <palomoshanecraven@gmail.com>
2025-06-22 18:02:19 +0800

    Add delete transaction and import transaction.py

--- bank.py
+++ bank.py
@@ -1,15 +1,26 @@
 # Includes all the information and etc.
+from transaction import Transaction
 
 class Bank:
     
-    pass
+    def __init__(self):
+        self.wallet = []
 
     # Add 
+    def add_transaction(self, transaction):
+        self.wallet.append(transaction)
 
     # Remove
+    def delete_transaction(self, title):
+        for occur in self.wallet:
+            if occur.title == title:
+                self.wallet.remove(occur)
+                return f"{title} has been removed."
+            return f"{title} is not found."
 
     # Display all
-
+    def display_information(self):
+        
     # Search
 
     # Save 
