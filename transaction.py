8f89bb8c3a9c829b85f1d8a6cdb16ce57e94ac04
Shane <palomoshanecraven@gmail.com>
2025-06-22 19:14:17 +0800

    Add comments in transaction.

--- transaction.py
+++ transaction.py
@@ -2,11 +2,13 @@
 
 class Transaction:
     
+    # Initialize necessarry variables.
     def __init__(self, title, amount, type, note = ""):
         self.title = title
         self.amount = amount
         self.type = type
         self.note = note
-
+    
+    # Display information.
     def display_info(self):
         return f"Transaction:\n Expense: {self.title}\n Amount: {self.amount}\n Type: {self.type}\n Note: {self.note}"
