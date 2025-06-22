02ce2102a5cbc38c4dc6d67fce2baa06c3c800de
Shane <palomoshanecraven@gmail.com>
2025-06-22 19:16:01 +0800

    Add comments in transaction.

--- bank.py
+++ bank.py
@@ -1,17 +1,20 @@
 # Includes all the information and etc.
+
+# Import transaction and json.
 from transaction import Transaction
 import json
 
 class Bank:
     
+    # Initialize empty list.
     def __init__(self):
         self.wallet = []
 
-    # Add 
+    # Add transaction.
     def add_transaction(self, transaction):
         self.wallet.append(transaction)
 
-    # Remove
+    # Remove transaction.
     def delete_transaction(self, title):
         for occur in self.wallet:
             if occur.title == title:
@@ -19,27 +22,27 @@ class Bank:
                 return f"{title} has been removed."
             return f"{title} is not found."
 
-    # Display all
+    # Display all information.
     def display_information(self):
         if not self.wallet:
             return "No transaction has been made in your bank."
         return "\n".join([transaction.display_info() for transaction in self.wallet])
 
-    # Search
+    # Search certain transaction.
     def search_wallet(self, query):
         found = [trans for trans in self.wallet if query.lower() in trans.title.lower() or query.lower() in trans.type.lower()]
         if not found:
             return "No transaction has been made."
         return "\n".join([transaction.display_info() for transaction in found])
     
-    # Save 
+    # Save transaction.
     def save_information(self, file_name = "wallet.json"):
         data = [{"title": transaction.title, "amount": transaction.amount, "type": transaction.type, "note": transaction.note } for transaction in self.wallet]
         with open(file_name, "w") as file:
             json.dump(data, file, indent = 4)
 
 
-    # Load
+    # Load transaction.
     def load_bank(self, file_name = "wallet.json"):
         try:
             with open(file_name, "r") as file:
