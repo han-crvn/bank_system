7c684297b32a3c7104c6ffa3d4e00305d17d72a7
Shane <palomoshanecraven@gmail.com>
2025-06-22 17:53:59 +0800

    Add short notes.

--- bank.py
+++ bank.py
@@ -2,11 +2,16 @@
 
 class Bank:
     
-    def __init__(self, title, amount, type, note = ""):
-        self.title = title
-        self.amount = amount
-        self.type = type
-        self.note = note
+    pass
 
-    def display_info(self):
-        return "Transaction:\n Expense: {self.title}\n Amount: {self.amount}\n Type: {self.type}\n Note: {self.note}"
+    # Add 
+
+    # Remove
+
+    # Display all
+
+    # Search
+
+    # Save 
+
+    # Load
